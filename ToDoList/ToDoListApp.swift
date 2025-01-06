974481d7a447e122aadada59a365247afdec759b
Henry Stephen <HenryStephen@163.com>
2025-01-06 20:15:09 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Henry Stephen on 2025/1/6.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
