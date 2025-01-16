ac3f9d29ce311aaa68292af4256065e3651b6e2c
Henry Stephen <HenryStephen@163.com>
2025-01-16 14:26:40 +0800

    initial commit

---
+++ ToDoList/Views/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  ToDoList
+//
+//  Created by Henry Stephen on 2025/1/15.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ ToDoList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  ToDoList
+//
+//  Created by Henry Stephen on 2025/1/6.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
